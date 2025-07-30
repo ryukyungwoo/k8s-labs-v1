9e899de3b0583bcd35608cea12f5706c85ac4f07
ryukyungwoo <ryukyungwoo1220@gmail.com>
2025-07-30 14:15:46 +0900

    k8s_day_1

--- apps/ddoddo-market/frontend/next.config.ts
+++ apps/ddoddo-market/frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // 빌드 중 ESLint 에러 무시
+  },
 };
 
 export default nextConfig;
